c952a5d9bcfd928fcb70bdbe8e4b75f21298cb0a
kazuki.yoshida <kazuki_6499@yahoo.co.jp>
2021-09-11 23:03:55 +0900

    cooltime導入／表示を更新

--- js/main.js
+++ js/main.js
@@ -4,27 +4,30 @@ const HEIGHT = 100;
 const CHARACTER_TYPES = {
     r: {
         type: "r",
-        life: 100,
-        attack: 100,
+        life: 50,
+        attack: 50,
         range: 10,
+        cooltime: 10,
         cost: 500,
         destination: WIDTH,
         color: "red",
     },
     g: {
         type: "g",
-        life: 400,
-        attack: 50,
+        life: 200,
+        attack: 100,
         range: 20,
+        cooltime: 10,
         cost: 1000,
         destination: WIDTH - 20,
         color: "green",
     },
     y: {
         type: "y",
-        life: 200,
-        attack: 10,
+        life: 100,
+        attack: 50,
         range: 40,
+        cooltime: 10,
         cost: 1000,
         destination: WIDTH - 40,
         color: "yellow",
@@ -44,8 +47,8 @@ var timer = setInterval(function () {
     } else if (state.time % 2 == 1) {
         attack_enemy();
         attack_area();
-        state.teamA.money += 10;
-        state.teamB.money += 10;
+        state.teamA.money += 50;
+        state.teamB.money += 50;
     }
 
     view();
@@ -60,11 +63,11 @@ var timer = setInterval(function () {
     }
 
     state.time++;
-}, 10);
+}, 20);
 
 function init() {
     var selectTeamA = document.getElementById("teamA.select");
-    for (var i of ["random", "sample"]) {
+    for (var i of ["sample", "random"]) {
         var optionElement = document.createElement("option");
         optionElement.textContent = i;
         optionElement.value = i;
@@ -92,6 +95,8 @@ function restart() {
             money: 1000,
             characters : [],
         },
+        fired: [],
+        bursted: [],
         winner: null,
     }
 }
@@ -116,8 +121,26 @@ function view() {
 
     for (var character of state.teamA.characters.concat(state.teamB.characters)) {
         ctx.fillStyle = CHARACTER_TYPES[character.type].color;
-        ctx.fillRect(5 * character.x, 5 * character.y, 5, 50);
+        ctx.fillRect(5 * character.x, 5 * character.y + 15, 20, 20);
     }
+
+    for (var fired of state.fired) {
+        ctx.beginPath();
+        ctx.moveTo(5 * fired[0], 5 * fired[1] + 25);
+        ctx.lineTo(5 * fired[2], 5 * fired[3] + 25);
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
+    state.fired = [];
+
+    for (var bursted of state.bursted) {
+        ctx.beginPath();
+        ctx.arc(5 * bursted[0], 5 * bursted[1] + 25, 100, 0 * Math.PI / 180, 360 * Math.PI / 180,);
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+    state.bursted = [];
 }
 
 function move() {
@@ -132,34 +155,42 @@ function move() {
 
 function attack_enemy() {
     for (var characterA of state.teamA.characters) {
-        var targets = [];
-        for (var characterB of state.teamB.characters) {
-            if (characterA.x <= characterB.x
-                && characterB.x <= characterA.x + characterA.range
-                && Math.abs(characterA.y - characterB.y) <= 10
-            ) {
-                targets.push(characterB);
+        if (state.time > characterA.fired + characterA.cooltime) {
+            var targets = [];
+            for (var characterB of state.teamB.characters) {
+                if (characterA.x <= characterB.x
+                    && characterB.x <= characterA.x + characterA.range
+                    && Math.abs(characterA.y - characterB.y) <= 10
+                ) {
+                    targets.push(characterB);
+                }
             }
-        }
-        if (targets.length > 0) {
-            var target = Math.floor(Math.random() * targets.length);
-            targets[target].life -= characterA.attack;
+            if (targets.length > 0) {
+                var target = Math.floor(Math.random() * targets.length);
+                targets[target].life -= characterA.attack;
+                characterA.fired = state.time;
+                state.fired.push([characterA.x, characterA.y, targets[target].x, targets[target].y]);
+            }
         }
     }
 
     for (var characterB of state.teamB.characters) {
-        var targets = [];
-        for (var characterA of state.teamA.characters) {
-            if (characterA.x <= characterB.x
-                && characterB.x - characterB.range <= characterA.x
-                && Math.abs(characterA.y - characterB.y) <= 10
-            ) {
-                targets.push(characterA);
+        if (state.time > characterB.fired + characterB.cooltime) {
+            var targets = [];
+            for (var characterA of state.teamA.characters) {
+                if (characterA.x <= characterB.x
+                    && characterB.x - characterB.range <= characterA.x
+                    && Math.abs(characterA.y - characterB.y) <= 10
+                ) {
+                    targets.push(characterA);
+                }
             }
-        }
-        if (targets.length > 0) {
-            var target = Math.floor(Math.random() * targets.length);
-            targets[target].life -= characterB.attack;
+            if (targets.length > 0) {
+                var target = Math.floor(Math.random() * targets.length);
+                targets[target].life -= characterB.attack;
+                characterB.fired = state.time;
+                state.fired.push([characterB.x, characterB.y, targets[target].x, targets[target].y]);
+            }
         }
     }
 
@@ -179,6 +210,7 @@ function attack_area() {
     for (var i = state.teamA.characters.length - 1; i >= 0; i--) {
         if (state.teamA.characters[i].type == "r" && state.teamA.characters[i].x >= WIDTH - 1) {
             state.teamB.life -= state.teamA.characters[i].attack;
+            state.bursted.push([state.teamA.characters[i].x, state.teamA.characters[i].y]);
             state.teamA.characters.splice(i, 1);
         }
     }
@@ -186,6 +218,7 @@ function attack_area() {
     for (var i = state.teamB.characters.length - 1; i >= 0; i--) {
         if (state.teamB.characters[i].type == "r" && state.teamB.characters[i].x <= 0) {
             state.teamA.life -= state.teamB.characters[i].attack;
+            state.bursted.push([state.teamB.characters[i].x, state.teamB.characters[i].y]);
             state.teamB.characters.splice(i, 1);
         }
     }
@@ -240,6 +273,8 @@ function generate(team, type, y) {
             attack: CHARACTER_TYPES[type].attack,
             range: CHARACTER_TYPES[type].range,
             destination: destination,
+            cooltime: CHARACTER_TYPES[type].cooltime,
+            fired: -100,
             x: x,
             y: y,
         });
--- js/tactics/random.js
+++ js/tactics/random.js
@@ -3,6 +3,10 @@ export class Tactics {
         var height = 100;
         var types = ["r", "g", "y"];
 
+        if (friend.money < 1000) {
+            return [];
+        }
+
         return [
             [Math.floor(Math.random() * height), types[Math.floor(Math.random() * types.length)]],
         ]
